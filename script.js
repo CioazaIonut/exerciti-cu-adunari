4141acbfbe17dcfc6532728681fddb6d9b9745dd
CioazaIoan <cioazai@yahoo.com>
2025-10-17 19:51:39 +0300

    mesaj

---
+++ script.js
@@ -0,0 +1,32 @@
+// Adaugă două numere și afișează rezultatul:
+
+let a = 5;
+let b = 10;
+console.log(a + b);
+
+// Calculază suma variabilelor pentru prețurile a două produse:
+
+let pretProdus1 = 25;
+let pretProdus2 = 40;
+let total = pretProdus1 + pretProdus2;
+console.log("Totalul este: " + total);
+
+// Adaugă un număr la o variabilă și afișează noua valoare:
+
+let numar = 7;
+numar = numar + 3;
+console.log(numar);
+
+// Fă suma a trei variabile și afișează rezultatul:
+
+let x = 2;
+let y = 4;
+let z = 6;
+let suma = x + y + z;
+console.log(suma);
+
+// Adună și concatenează două string-uri:
+
+let salut = "Bună ";
+let nume = "Andrei";
+console.log(salut + nume);
